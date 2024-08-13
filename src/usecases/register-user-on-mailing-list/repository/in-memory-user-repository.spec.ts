a43b2d5a7d7665a617aac7090dc2e12736dcc8c5
Raquel Marangoni <rmarangonisan@gmail.com>
2024-08-12 21:10:03 -0300

    test: add and find user and check its existence

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { UserData } from '../user-data'
-import { InMemoryUserRepository } from './in-memory-user'
+import { InMemoryUserRepository } from './in-memory-user-repository'
 
 describe('In memory User repository', () => {
   test('Should return null if user is not found', async () => {
@@ -7,5 +7,15 @@ describe('In memory User repository', () => {
     const userRepo = new InMemoryUserRepository(users)
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
+  })
+
+  test('should return user if is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@mail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@mail.com')
+    expect(user.name).toBe('any_name')
   })
 })
