45bb74b5748f8be789880398c5eacc725766fead
Raquel Marangoni <rmarangonisan@gmail.com>
2024-08-12 21:09:39 -0300

    feat: add and find user and check its existence

---
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -0,0 +1,38 @@
+import { UserRepository } from '../ports/users-repository'
+import { UserData } from '../user-data'
+
+export class InMemoryUserRepository implements UserRepository {
+  private repository: UserData[]
+
+  constructor (repository:UserData[]) {
+    this.repository = repository
+  }
+
+  async add (user: UserData): Promise<void> {
+    const exists = await this.exists(user)
+    if (!exists) {
+      this.repository.push(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const users = this.repository.filter((user) => {
+      return user.email === email
+    })
+    if (users.length > 0) {
+      return users[0]
+    }
+    return null
+  }
+
+  async finsAllusers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    if (await this.findUserByEmail(user.email) === null) {
+      return false
+    }
+    return true
+  }
+}
